9af7b968e6e3572513d4088fb114f1e6ae5163d6
OKAN CANDENİZ <candenizokan@gmail.com>
2023-08-06 12:11:46 +0300

    Product model

--- AsenkronProgramlama/Models/Entities/Concrete/Product.cs
+++ AsenkronProgramlama/Models/Entities/Concrete/Product.cs
@@ -1,6 +1,17 @@
-﻿namespace AsenkronProgramlama.Models.Entities.Concrete
+﻿using AsenkronProgramlama.Models.Entities.Abstract;
+
+namespace AsenkronProgramlama.Models.Entities.Concrete
 {
-    public class Product
+    public class Product :BaseEntity
     {
+        public string Name { get; set; }
+        public string Slug { get; set; }
+        public string Description { get; set; }
+        public int Stock { get; set; }
+
+        //navigation
+        //bir productın bir karegorisi olur. bir id ve nesne barındırmam lazım
+        public int CategoryId { get; set; }
+        public virtual Category Category { get; set; }
     }
 }
