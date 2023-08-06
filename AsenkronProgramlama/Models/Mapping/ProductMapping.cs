b9fa96d8dbafd47fe457b866fb71b625350dcc80
OKAN CANDENİZ <candenizokan@gmail.com>
2023-08-06 12:24:00 +0300

    ilişkiyi açıkladım  builder.HasOne(a => a.Category).WithMany(a => a.Products).HasForeignKey(a => a.CategoryId);

--- AsenkronProgramlama/Models/Mapping/ProductMapping.cs
+++ AsenkronProgramlama/Models/Mapping/ProductMapping.cs
@@ -1,9 +1,14 @@
 ﻿using AsenkronProgramlama.Models.Entities.Concrete;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace AsenkronProgramlama.Models.Mapping
 {
-    public class ProductMapping:IEntityTypeConfiguration<Product>//IEntityTypeConfiguration için nugetten Microsoft.EntityFrameworkCore.SqlServer indirmem gerekiyor gitmişken Microsoft.EntityFrameworkCore.Tools da indiriyorum ki migrationu başlatayım
+    public class ProductMapping : IEntityTypeConfiguration<Product>//IEntityTypeConfiguration için nugetten Microsoft.EntityFrameworkCore.SqlServer indirmem gerekiyor gitmişken Microsoft.EntityFrameworkCore.Tools da indiriyorum ki migrationu başlatayım
     {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            builder.HasOne(a => a.Category).WithMany(a => a.Products).HasForeignKey(a => a.CategoryId); //bir ürünün bir kategorisi vardır
+        }
     }
 }
