f1847861f6b18b88438f699d3e40b5584b960ed7
OKAN CANDENİZ <candenizokan@gmail.com>
2023-08-06 13:18:57 +0300

    IBaseRepository

---
+++ AsenkronProgramlama/Infrastructure/Repositories/Interfaces/IBaseRepository.cs
@@ -0,0 +1,8 @@
+﻿using AsenkronProgramlama.Models.Entities.Abstract;
+
+namespace AsenkronProgramlama.Infrastructure.Repositories.Interfaces
+{
+    public interface IBaseRepository<T> where T : BaseEntity
+    {
+    }
+}
