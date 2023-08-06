97cf8b9835e1843acc2d30cc03107d3d521dbcf2
OKAN CANDENİZ <candenizokan@gmail.com>
2023-08-06 13:38:16 +0300

    BaseRepository implement interfaces

--- AsenkronProgramlama/Infrastructure/Repositories/Cocrete/BaseRepository.cs
+++ AsenkronProgramlama/Infrastructure/Repositories/Cocrete/BaseRepository.cs
@@ -1,9 +1,44 @@
 ﻿using AsenkronProgramlama.Infrastructure.Repositories.Interfaces;
 using AsenkronProgramlama.Models.Entities.Abstract;
+using Microsoft.EntityFrameworkCore.Query;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
 
 namespace AsenkronProgramlama.Infrastructure.Repositories.Cocrete
 {
-    public class BaseRepository<T>:IBaseRepository<T> where T : BaseEntity
+    public class BaseRepository<T> : IBaseRepository<T> where T : BaseEntity
     {
+        public Task Add(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> GetByDefault(Expression<Func<T, bool>> expression)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<T>> GetByDefaults(Expression<Func<T, bool>> expression)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<TResult>> GetFilter<TResult>(Expression<Func<T, TResult>> selector, Expression<Func<T, bool>> expression, Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null, Func<IQueryable<T>, IIncludableQueryable<T, object>> join = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Update(T entity)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
