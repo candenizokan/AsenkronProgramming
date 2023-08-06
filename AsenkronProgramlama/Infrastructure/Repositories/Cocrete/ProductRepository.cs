f698e6fbce45942e27f2e3b9b11166ff2316d8d5
OKAN CANDENİZ <candenizokan@gmail.com>
2023-08-06 14:03:40 +0300

    ctor ProductRepository(ApplicationDbContext context):base(context)

--- AsenkronProgramlama/Infrastructure/Repositories/Cocrete/ProductRepository.cs
+++ AsenkronProgramlama/Infrastructure/Repositories/Cocrete/ProductRepository.cs
@@ -1,9 +1,14 @@
-﻿using AsenkronProgramlama.Infrastructure.Repositories.Interfaces;
+﻿using AsenkronProgramlama.Infrastructure.Context;
+using AsenkronProgramlama.Infrastructure.Repositories.Interfaces;
 using AsenkronProgramlama.Models.Entities.Concrete;
 
 namespace AsenkronProgramlama.Infrastructure.Repositories.Cocrete
 {
     public class ProductRepository:BaseRepository<Product>,IProductRepository
     {
+        public ProductRepository(ApplicationDbContext context):base(context)
+        {
+
+        }
     }
 }
