e5729fc6a1f3fa0010ae32540bf597b5f7befc89
OKAN CANDENİZ <candenizokan@gmail.com>
2023-08-06 14:02:08 +0300

    ctor CategoryRepository(ApplicationDbContext context):base(context)

--- AsenkronProgramlama/Infrastructure/Repositories/Cocrete/CategoryRepository.cs
+++ AsenkronProgramlama/Infrastructure/Repositories/Cocrete/CategoryRepository.cs
@@ -1,9 +1,14 @@
-﻿using AsenkronProgramlama.Infrastructure.Repositories.Interfaces;
+﻿using AsenkronProgramlama.Infrastructure.Context;
+using AsenkronProgramlama.Infrastructure.Repositories.Interfaces;
 using AsenkronProgramlama.Models.Entities.Concrete;
 
 namespace AsenkronProgramlama.Infrastructure.Repositories.Cocrete
 {
     public class CategoryRepository:BaseRepository<Category>,ICategoryRepository
     {
+        public CategoryRepository(ApplicationDbContext context):base(context)
+        {
+
+        }
     }
 }
