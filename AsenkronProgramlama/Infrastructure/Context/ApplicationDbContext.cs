7a1022c41acfd526dba838a6b85d971bec228bc9
OKAN CANDENİZ <candenizokan@gmail.com>
2023-08-06 12:31:53 +0300

    ApplicationDbContext

--- AsenkronProgramlama/Infrastructure/Context/ApplicationDbContext.cs
+++ AsenkronProgramlama/Infrastructure/Context/ApplicationDbContext.cs
@@ -1,6 +1,27 @@
-﻿namespace AsenkronProgramlama.Infrastructure.Context
+﻿using AsenkronProgramlama.Models.Entities.Concrete;
+using AsenkronProgramlama.Models.Mapping;
+using Microsoft.EntityFrameworkCore;
+
+namespace AsenkronProgramlama.Infrastructure.Context
 {
-    public class ApplicationDbContext
+    public class ApplicationDbContext:DbContext
     {
+
+        //startupta konuşturmak için ctorda connectionstring 
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        //ayağa kalkmasını istediğim tabloları db sette yapıyorum
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
+        //konfigurayonlarım var
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new ProductMapping());
+            base.OnModelCreating(modelBuilder);
+        }
+
     }
 }
