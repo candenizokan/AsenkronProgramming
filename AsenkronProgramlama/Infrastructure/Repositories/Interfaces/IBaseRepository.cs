c2e3b643ad0792cd1d2a3b0d88eb8ba2760c8e06
OKAN CANDENİZ <candenizokan@gmail.com>
2023-08-06 13:28:12 +0300

    IBaseRepository

--- AsenkronProgramlama/Infrastructure/Repositories/Interfaces/IBaseRepository.cs
+++ AsenkronProgramlama/Infrastructure/Repositories/Interfaces/IBaseRepository.cs
@@ -1,8 +1,26 @@
 ﻿using AsenkronProgramlama.Models.Entities.Abstract;
+using Microsoft.EntityFrameworkCore.Query;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
 
 namespace AsenkronProgramlama.Infrastructure.Repositories.Interfaces
 {
     public interface IBaseRepository<T> where T : BaseEntity
     {
+
+        Task Add(T entity);
+        Task Update(T entity);
+        Task<T> GetById(int id);
+
+        Task<List<T>> GetByDefaults(Expression<Func<T,bool>> expression);
+        Task<T> GetByDefault(Expression<Func<T,bool>> expression);
+
+        Task<List<TResult>> GetFilter<TResult>(Expression<Func<T,TResult>> selector,
+                                               Expression<Func<T,bool>> expression
+                                               Func<IQueryable<T>,IOrderedQueryable<T>> orderBy=null,
+                                               Func<IQueryable<T>,IIncludableQueryable<T,object>> join=null);
     }
 }
