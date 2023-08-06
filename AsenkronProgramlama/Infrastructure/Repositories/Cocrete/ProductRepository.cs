2faa1eecd176e2c88a3ca5effef33c9e057db4a9
OKAN CANDENİZ <candenizokan@gmail.com>
2023-08-06 14:02:56 +0300

    ProductRepository:BaseRepository<Product>,IProductRepository

---
+++ AsenkronProgramlama/Infrastructure/Repositories/Cocrete/ProductRepository.cs
@@ -0,0 +1,9 @@
+﻿using AsenkronProgramlama.Infrastructure.Repositories.Interfaces;
+using AsenkronProgramlama.Models.Entities.Concrete;
+
+namespace AsenkronProgramlama.Infrastructure.Repositories.Cocrete
+{
+    public class ProductRepository:BaseRepository<Product>,IProductRepository
+    {
+    }
+}
