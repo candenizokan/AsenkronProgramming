5a8e2f3240e32183df8f4496c70def9d9eb27e90
OKAN CANDENİZ <candenizokan@gmail.com>
2023-08-06 11:43:43 +0300

    Add project files.

---
+++ AsenkronProgramlama/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AsenkronProgramlama.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
