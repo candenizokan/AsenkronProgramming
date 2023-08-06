00fcc0030bc0d53448a59819d89840251165367d
OKAN CANDENİZ <candenizokan@gmail.com>
2023-08-06 12:00:17 +0300

    baseentity

---
+++ AsenkronProgramlama/Models/Entities/Abstract/BaseEntity.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace AsenkronProgramlama.Models.Entities.Abstract
+{
+    public class BaseEntity
+    {
+        public int ID { get; set; }
+
+		private DateTime _createdDate = DateTime.Now;
+
+		public DateTime CreatedDate
+		{
+			get { return _createdDate; }
+			set { _createdDate = value; }
+		}
+
+		public DateTime? UpdateDate { get; set; }
+
+
+	}
+}
