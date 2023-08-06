cea8edd2145877c0150c0a108b5df5e54b716a5f
OKAN CANDENİZ <candenizokan@gmail.com>
2023-08-06 13:37:55 +0300

    BaseRepository<T>:IBaseRepository<T> where T : BaseEntity

--- AsenkronProgramlama/Infrastructure/Repositories/Cocrete/BaseRepository.cs
+++ AsenkronProgramlama/Infrastructure/Repositories/Cocrete/BaseRepository.cs
@@ -1,6 +1,9 @@
-﻿namespace AsenkronProgramlama.Infrastructure.Repositories.Cocrete
+﻿using AsenkronProgramlama.Infrastructure.Repositories.Interfaces;
+using AsenkronProgramlama.Models.Entities.Abstract;
+
+namespace AsenkronProgramlama.Infrastructure.Repositories.Cocrete
 {
-    public class BaseRepository
+    public class BaseRepository<T>:IBaseRepository<T> where T : BaseEntity
     {
     }
 }
