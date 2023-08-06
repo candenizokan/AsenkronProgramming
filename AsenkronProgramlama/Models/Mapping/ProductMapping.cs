e6b0e266f39ebdd0c9ae18883bc58522eaed0c5a
OKAN CANDENİZ <candenizokan@gmail.com>
2023-08-06 12:22:03 +0300

    add nuget

--- AsenkronProgramlama/Models/Mapping/ProductMapping.cs
+++ AsenkronProgramlama/Models/Mapping/ProductMapping.cs
@@ -1,6 +1,9 @@
-﻿namespace AsenkronProgramlama.Models.Mapping
+﻿using AsenkronProgramlama.Models.Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+
+namespace AsenkronProgramlama.Models.Mapping
 {
-    public class ProductMapping
+    public class ProductMapping:IEntityTypeConfiguration<Product>//IEntityTypeConfiguration için nugetten Microsoft.EntityFrameworkCore.SqlServer indirmem gerekiyor gitmişken Microsoft.EntityFrameworkCore.Tools da indiriyorum ki migrationu başlatayım
     {
     }
 }
