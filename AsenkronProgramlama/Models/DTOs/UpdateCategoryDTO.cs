166358a39211397b93df1fb32703f18aaa7ee67b
Okan Candeniz <candenizokan@gmail.com>
2023-08-12 13:26:30 +0300

    UpdateCategoryDTO

--- AsenkronProgramlama/Models/DTOs/UpdateCategoryDTO.cs
+++ AsenkronProgramlama/Models/DTOs/UpdateCategoryDTO.cs
@@ -1,6 +1,16 @@
-﻿namespace AsenkronProgramlama.Models.DTOs
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AsenkronProgramlama.Models.DTOs
 {
     public class UpdateCategoryDTO
     {
+        //kimi güncelleyeceğimi bilmek için ID bilgisini forma gömeceğim ve esas post'ta kullanacağım
+        public int ID { get; set; }
+
+        [Required(ErrorMessage = "Bu alan boş bırakılamaz")] // boş bırakılamaz
+        [MinLength(3, ErrorMessage = "İsim en az 3 karakter olmalı")] //min karakter sayısı
+        public string Name { get; set; }
+
+        public string Slug => Name.ToLower().Replace(' ', '-');
     }
 }
