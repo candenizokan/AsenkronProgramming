1bb2b4d87c2ee991d0533d5d62d20f3552060df7
Okan Candeniz <candenizokan@gmail.com>
2023-08-12 11:48:00 +0300

    CreateMap<CreateCategoryDTO, Category>();

--- AsenkronProgramlama/Infrastructure/AutoMappers/Mappers.cs
+++ AsenkronProgramlama/Infrastructure/AutoMappers/Mappers.cs
@@ -1,8 +1,14 @@
-﻿using AutoMapper;
+﻿using AsenkronProgramlama.Models.DTOs;
+using AsenkronProgramlama.Models.Entities.Concrete;
+using AutoMapper;
 
 namespace AsenkronProgramlama.Infrastructure.AutoMappers
 {
     public class Mappers:Profile
     {
+        public Mappers()
+        {
+            CreateMap<CreateCategoryDTO, Category>();
+        }
     }
 }
