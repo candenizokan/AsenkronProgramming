484b9e3c035074b8842f10225e57fd4630077743
Okan Candeniz <candenizokan@gmail.com>
2023-08-12 10:50:20 +0300

    CreateCategoryDTO

--- AsenkronProgramlama/Models/DTOs/CreateCategoryDTO.cs
+++ AsenkronProgramlama/Models/DTOs/CreateCategoryDTO.cs
@@ -1,6 +1,13 @@
-﻿namespace AsenkronProgramlama.Models.DTOs
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AsenkronProgramlama.Models.DTOs
 {
     public class CreateCategoryDTO
     {
+        [Required(ErrorMessage = "Bu alan boş bırakılamaz")] // boş bırakılamaz
+        [MinLength(3, ErrorMessage = "İsim en az 3 karakter olmalı")] //min karakter sayısı
+        public string Name { get; set; }
+
+        public string Slug => Name.ToLower().Replace(' ', '-');
     }
 }
