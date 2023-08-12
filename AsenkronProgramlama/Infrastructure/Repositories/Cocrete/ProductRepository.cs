1cef9d1dfcf05ea0e4b259ce5f0895df3773cfe6
Okan Candeniz <candenizokan@gmail.com>
2023-08-12 10:44:08 +0300

    ctor note

--- AsenkronProgramlama/Infrastructure/Repositories/Cocrete/ProductRepository.cs
+++ AsenkronProgramlama/Infrastructure/Repositories/Cocrete/ProductRepository.cs
@@ -7,7 +7,7 @@ namespace AsenkronProgramlama.Infrastructure.Repositories.Cocrete
     public class ProductRepository:BaseRepository<Product>,IProductRepository
     {
         public ProductRepository(ApplicationDbContext context):base(context)
-        {
+        {//atasına gönderiyor base'e iş yaptırmak için
 
         }
     }
