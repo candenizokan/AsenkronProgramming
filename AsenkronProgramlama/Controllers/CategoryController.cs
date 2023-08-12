36fcb2948eae44018f9321cbb51a9824b578d8fc
Okan Candeniz <candenizokan@gmail.com>
2023-08-12 10:45:26 +0300

    add CategoryController

---
+++ AsenkronProgramlama/Controllers/CategoryController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AsenkronProgramlama.Controllers
+{
+    public class CategoryController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
