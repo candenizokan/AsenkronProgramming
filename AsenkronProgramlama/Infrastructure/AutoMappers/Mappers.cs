a85642597b7e5a0cd28bc8f7055833def82711e4
Okan Candeniz <candenizokan@gmail.com>
2023-08-12 12:08:53 +0300

    //ignore

--- AsenkronProgramlama/Infrastructure/AutoMappers/Mappers.cs
+++ AsenkronProgramlama/Infrastructure/AutoMappers/Mappers.cs
@@ -10,7 +10,7 @@ namespace AsenkronProgramlama.Infrastructure.AutoMappers
         {
             CreateMap<CreateCategoryDTO, Category>().ReverseMap();//CreateCategoryDTO alanlara bak bana Category nesne ver.ReverseMap() eşleşmenin iki yönlü çalışması
 
-            CreateMap<CreateCategoryDTO, Category>().ReverseMap().ForMember(a=>a.Slug,opt=>opt.Ignore());//=>Ignore et slug taşıma
+            //CreateMap<CreateCategoryDTO, Category>().ReverseMap().ForMember(a=>a.Slug,opt=>opt.Ignore());//=>Ignore et slug taşıma
 
         }
     }
