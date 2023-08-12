9f59c8ed5651147ded82bfe9ecb443d02bdf6d86
Okan Candeniz <candenizokan@gmail.com>
2023-08-12 11:50:55 +0300

    ReverseMap

--- AsenkronProgramlama/Infrastructure/AutoMappers/Mappers.cs
+++ AsenkronProgramlama/Infrastructure/AutoMappers/Mappers.cs
@@ -8,7 +8,7 @@ namespace AsenkronProgramlama.Infrastructure.AutoMappers
     {
         public Mappers()
         {
-            CreateMap<CreateCategoryDTO, Category>();//CreateCategoryDTO alanlara bak bana Category nesne ver
+            CreateMap<CreateCategoryDTO, Category>().ReverseMap();//CreateCategoryDTO alanlara bak bana Category nesne ver.ReverseMap() eşleşmenin iki yönlü çalışması
         }
     }
 }
