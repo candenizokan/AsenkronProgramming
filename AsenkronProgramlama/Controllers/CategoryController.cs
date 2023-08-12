a1005feeed838f55f9621c5d138fdcc8c319d684
Okan Candeniz <candenizokan@gmail.com>
2023-08-12 11:04:24 +0300

    Create [HttpPost]

--- AsenkronProgramlama/Controllers/CategoryController.cs
+++ AsenkronProgramlama/Controllers/CategoryController.cs
@@ -1,10 +1,17 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AsenkronProgramlama.Models.DTOs;
+using Microsoft.AspNetCore.Mvc;
 
 namespace AsenkronProgramlama.Controllers
 {
     public class CategoryController : Controller
     {
         public IActionResult Create() => View();
+
+        [HttpPost]
+        public IActionResult Create(CreateCategoryDTO dto)
+        {
+            return View(dto);
+        }
         
     }
 }
