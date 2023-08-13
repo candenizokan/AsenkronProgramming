1955b6aa6b2642287cddb29d56ea45dbbf9b2a89
Okan Candeniz <candenizokan@gmail.com>
2023-08-13 12:07:16 +0300

    add ProductVM class

---
+++ AsenkronProgramlama/Models/VMs/ProductVM.cs
@@ -0,0 +1,14 @@
+﻿using AsenkronProgramlama.Models.Enums;
+
+namespace AsenkronProgramlama.Models.VMs
+{
+    public class ProductVM
+    {
+        public int ProductID { get; set; }
+        public string Name { get; set; }
+        public Statu Statu { get; set; }
+        public int Stock { get; set; }
+        //categori için
+        public string CategoryName { get; set; }
+    }
+}
