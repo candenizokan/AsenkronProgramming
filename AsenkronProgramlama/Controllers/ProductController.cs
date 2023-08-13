26aa7c9c2f72b81190f8ab20a893123907b001ef
Okan Candeniz <candenizokan@gmail.com>
2023-08-13 10:33:53 +0300

    ViewBag.Categories =  new SelectList note

--- AsenkronProgramlama/Controllers/ProductController.cs
+++ AsenkronProgramlama/Controllers/ProductController.cs
@@ -2,6 +2,7 @@
 using AsenkronProgramlama.Models.Enums;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Threading.Tasks;
 
 namespace AsenkronProgramlama.Controllers
 {
@@ -14,10 +15,12 @@ namespace AsenkronProgramlama.Controllers
             _categoryRepository = categoryRepository;
         }
 
-        public IActionResult Create()
+        public async Task<IActionResult> Create()
         {
             //bura category repoya ihtiyacım doğdu. bunu ctorda di ile alacağım
-            ViewBag.Categories = new SelectListItem(_categoryRepository.GetByDefaults(a => a.Statu != Statu.Passive), "ID", "Name");//pasive olmayan tüm kategorileri göndermem lazım
+
+            //Not: normalde SelectListItem nesnelerini bir listeye doldurarak ve her bir kategori nesnesinden bir SelectListItem nesnesi oluşturarak da bu işlemi yapabilirdik. buda farklı bir yöntem olarak burada kalsın
+            ViewBag.Categories =  new SelectList(await _categoryRepository.GetByDefaults(a => a.Statu != Statu.Passive), "ID", "Name");//pasive olmayan tüm kategorileri göndermem lazım
         }
     }
 }
