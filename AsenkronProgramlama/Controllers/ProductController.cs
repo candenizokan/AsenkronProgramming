bb80a415abf699e591745f53a442b93d6296eb4a
Okan Candeniz <candenizokan@gmail.com>
2023-08-13 10:24:01 +0300

    ctor ile _categoryRepository ye ulaşıyorum

--- AsenkronProgramlama/Controllers/ProductController.cs
+++ AsenkronProgramlama/Controllers/ProductController.cs
@@ -1,9 +1,17 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AsenkronProgramlama.Infrastructure.Repositories.Interfaces;
+using Microsoft.AspNetCore.Mvc;
 
 namespace AsenkronProgramlama.Controllers
 {
     public class ProductController : Controller
     {
+        private readonly ICategoryRepository _categoryRepository;
+
+        public ProductController(ICategoryRepository categoryRepository)
+        {
+            _categoryRepository = categoryRepository;
+        }
+
         public IActionResult Create()
         {
             ViewBag.Categories=//bura category repoya ihtiyacım doğdu. bunu ctorda di ile alacağım
