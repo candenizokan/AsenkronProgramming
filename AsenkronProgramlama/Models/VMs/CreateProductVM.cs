e1f3b859df73d60bdd2c7fa0f15df772d33a50b7
Okan Candeniz <candenizokan@gmail.com>
2023-08-13 10:17:59 +0300

    CreateProductVM

---
+++ AsenkronProgramlama/Models/VMs/CreateProductVM.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AsenkronProgramlama.Models.VMs
+{
+    public class CreateProductVM
+    {
+        [Required(ErrorMessage = "Bu alan boş bırakılamaz")]
+        public string Name { get; set; }
+        
+        [Required(ErrorMessage = "Bu alan boş bırakılamaz")]
+        public int Stock { get; set; }
+
+        [Required(ErrorMessage = "Bu alan boş bırakılamaz")]
+        public string Description { get; set; }
+        public string Slug => Name.ToLower().Replace(' ', '-');
+
+        //categori için
+
+        [Required(ErrorMessage = "Lütfen Kategori Seçiniz")]
+        public int CategoryID { get; set; }
+    }
+}
