f582396d0d0a39091b30b01f980f284ddd4f8cce
Okan Candeniz <candenizokan@gmail.com>
2023-08-13 10:36:48 +0300

    Create()

--- AsenkronProgramlama/Controllers/ProductController.cs
+++ AsenkronProgramlama/Controllers/ProductController.cs
@@ -21,6 +21,8 @@ namespace AsenkronProgramlama.Controllers
 
             //Not: normalde SelectListItem nesnelerini bir listeye doldurarak ve her bir kategori nesnesinden bir SelectListItem nesnesi oluşturarak da bu işlemi yapabilirdik. buda farklı bir yöntem olarak burada kalsın
             ViewBag.Categories =  new SelectList(await _categoryRepository.GetByDefaults(a => a.Statu != Statu.Passive), "ID", "Name");//pasive olmayan tüm kategorileri göndermem lazım listeyi dolduruken içerdeki id yi id kabul et namei ni text kabul et gibi düşün.
+
+            return View();
         }
     }
 }
